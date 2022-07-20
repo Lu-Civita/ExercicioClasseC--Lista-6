6c6fd1aa5c65522e4b13570014e67ad7c0527813
lu.civita <analouisecotrim@gmail.com>
2022-07-19 22:10:25 -0300

    Classe - LojaRoupa

---
+++ Solution1/Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using Classe.Src;
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LojaRoupa legging = new LojaRoupa(Categoria.FITNESS, "legging", "preta", "inverno");
+            LojaRoupa pulseira = new LojaRoupa(Categoria.ACESSORIOS, "pulseira", "dourada", "verão");
+            LojaRoupa sandalia = new LojaRoupa(Categoria.CALCADO, "sandália", "prateada", "verão");
+            LojaRoupa sutia = new LojaRoupa(Categoria.LINGERIE, "sutiã", "branco", "verão");
+            LojaRoupa sunga = new LojaRoupa(Categoria.MODAPRAIA, "sunga", "azul-marinho", "verão");
+
+            Console.WriteLine(legging.ToString());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(pulseira.ToString());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(sandalia.ToString());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(sutia.ToString());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(sunga.ToString());
+
+        }
+    }
+}
---
+++ Solution1/Program/Src/LojaRoupa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe.Src
+{
+    public class LojaRoupa
+    {
+        Categoria Tipo { get; set; }
+        string Nome { get; set; }
+        string Cor { get; set; }
+        string Estacao { get; set; }
+
+        public LojaRoupa(Categoria tipo, string nome, string cor, string estacao)
+        {
+            Tipo = tipo;
+            Nome = nome;
+            Cor = cor;
+            Estacao = estacao;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"\n Nome da Peça de roupa: {Nome}" +
+                $"\n Tipo da Peça de roupa: {Tipo}" +
+                $"\n Cor da Peça de roupa: {Cor}" +
+                $"\n Estação da Peça de roupa: {Estacao}";
+        }
+
+    }
+
+    public enum Categoria
+    {
+        FITNESS,
+        ACESSORIOS,
+        CALCADO,
+        LINGERIE,
+        MODAPRAIA,
+    }
+}
